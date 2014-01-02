b322d2ff0a2e8ab7e3639481ce345c7709417881
Riza Fahmi <rizafahmi@gmail.com>
2014-01-02 11:33:40 +0700

    New version of our app is here. Remove unwanted files.

--- lib/typecheckerapp.ex
+++ lib/typecheckerapp.ex
@@ -7,21 +7,24 @@ defmodule Typecheckerapp do
     Typecheckerapp.Supervisor.start_link
   end
 
-  def receive_variable(variable) do
+  def receive_and_check(), do: {:error, "No data"}
+
+  def receive_and_check(variable) do
+    {:ok, received_var} = receive_variable(variable)
+    received_var |> check_variable
+  end
+
+  defp receive_variable(variable) do
     {:ok, variable}
   end
 
-  def receive_variable() do
+  defp receive_variable() do
     {:error, "No data"}
   end
 
-  def check_variable(variable) do
-    cond do
-      is_integer(variable) == true -> {:ok, "You give us an integer"}
-      is_float(variable) == true -> {:ok, "You give us a float"}
-      is_tuple(variable) == true -> {:ok, "You give us a tuple"}
-      is_list(variable) == true -> {:ok, "You give us a list"}
-      is_atom(variable) == true -> {:ok, "You give us an atom"}
-    end
-  end
+  defp check_variable(variable) when is_integer(variable), do: {:ok, "You give us an integer"}
+  defp check_variable(variable) when is_float(variable),   do: {:ok, "You give us a float"}
+  defp check_variable(variable) when is_tuple(variable),   do: {:ok, "You give us a tuple"}
+  defp check_variable(variable) when is_list(variable),    do: {:ok, "You give us a list"}
+  defp check_variable(variable) when is_atom(variable),    do: {:ok, "You give us an atom"}
 end
--- test/typecheckerapp_test.exs
+++ test/typecheckerapp_test.exs
@@ -1,40 +1,29 @@
 defmodule TypecheckerappTest do
   use ExUnit.Case
 
-  test "receive any data type" do
-    assert Typecheckerapp.receive_variable(25) == {:ok, 25}
-    assert Typecheckerapp.receive_variable([1, 2, 3]) == {:ok, [1, 2, 3]}
-    assert Typecheckerapp.receive_variable([]) == {:ok, []}
-    assert Typecheckerapp.receive_variable({1, 2, 3}) == {:ok, {1, 2, 3}}
-    assert Typecheckerapp.receive_variable(:data) == {:ok, :data}
-  end
-  test "did not receive any data" do
-    assert Typecheckerapp.receive_variable == {:error, "No data"}
-  end
-  test "check variable is integer" do
-    {:ok, variable} = Typecheckerapp.receive_variable(25)
-    assert Typecheckerapp.check_variable(variable) == {:ok, "You give us an integer"}
-  end
-  test "check variable is float" do
-    {:ok, variable} = Typecheckerapp.receive_variable(25.12)
-    assert Typecheckerapp.check_variable(variable) == {:ok, "You give us a float"}
-  end
-  test "check variable is tuple" do
-    {:ok, variable} = Typecheckerapp.receive_variable({})
-    assert Typecheckerapp.check_variable(variable) == {:ok, "You give us a tuple"}
+  # Encore Part
+  test "testing receive and check is integer" do
+    assert Typecheckerapp.receive_and_check(12) == {:ok, "You give us an integer"}
   end
 
-  test "check variable is list" do
-    {:ok, variable} = Typecheckerapp.receive_variable([1, 2])
-    assert Typecheckerapp.check_variable(variable) == {:ok, "You give us a list"}
+  test "testing receive and check is float" do
+    assert Typecheckerapp.receive_and_check(1.2) == {:ok, "You give us a float"}
   end
 
-  test "check variable is an atom" do
-    {:ok, variable} = Typecheckerapp.receive_variable(:some_atom)
-    assert Typecheckerapp.check_variable(variable) == {:ok, "You give us an atom"}
+  test "testing receive and check is tuple" do
+    assert Typecheckerapp.receive_and_check({}) == {:ok, "You give us a tuple"}
   end
 
-  test "check blank variable" do
-    {:error, "No data"} = Typecheckerapp.receive_variable
+  test "testing receive and check is list" do
+    assert Typecheckerapp.receive_and_check([1,2]) == {:ok, "You give us a list"}
   end
+
+  test "testing receive and check is atom" do
+    assert Typecheckerapp.receive_and_check(:one_little_atom) == {:ok, "You give us an atom"}
+  end
+
+  test "testing receive and check is blank" do
+    assert Typecheckerapp.receive_and_check() == {:error, "No data"}
+  end
+
 end
